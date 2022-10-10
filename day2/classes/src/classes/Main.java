fd333a396961eaec258271e534d538be54cdd1e2
ipekozbay <20060401@stu.omu.edu.tr>
2022-10-10 17:26:21 +0300

    day2 - referance type

--- day2/classes/src/classes/Main.java
+++ day2/classes/src/classes/Main.java
@@ -17,7 +17,16 @@ public class Main {
 		
 		num2=num1;
 		num1=30;
-		System.out.print(num2);
+		System.out.println(num2);
+		
+		int [] numbers1 = new int [] {1,2,3};
+		int [] numbers2 = new int [] {4,5,6};
+
+		numbers2=numbers1;
+		numbers1[0] = 10;
+		
+		System.out.println(numbers2[0]);
+		
 	}
 
 }
