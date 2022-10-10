02c50cc737ae7d25c607aa71100ff22e62e36b44
ipekozbay <20060401@stu.omu.edu.tr>
2022-10-10 18:24:05 +0300

    day3-inheritance and method overloading

---
+++ day3/methodOverloading/src/methodOverloading/FourtTansactions.java
@@ -0,0 +1,15 @@
+package methodOverloading;
+
+public class FourtTansactions {
+	
+	public int Addition(int num1,int num2) {
+		
+		return num1 + num2;
+	}
+	
+	public int Addition(int num1,int num2, int num3) {
+		
+		return num1 + num2;
+	}
+
+}
---
+++ day3/methodOverloading/src/methodOverloading/Main.java
@@ -0,0 +1,12 @@
+package methodOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FourtTansactions fourtTansactions =new FourtTansactions();
+		fourtTansactions.Addition(1, 2);
+		fourtTansactions.Addition(2, 3, 3);
+	}
+	
+
+}
